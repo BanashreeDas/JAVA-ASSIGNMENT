41fc670be29de94de0b989fd14833aaedd22b63c
BanashreeDas <banashreed2@gmail.com>
2025-03-09 13:30:15 +0530

    Update and rename Q4.java to Question4.java

--- Question4.java
+++ Question4.java
@@ -2,29 +2,29 @@
 
 import java.io.*;
 import java.util.*;
-class seq{
-    void even(int n){
-        int n1=0, n2=1, n3, i;
+class JavaSequence{
+    void EVEN(int n){
+        int number1=0, number2=1, number3, i;
         System.out.print("Even and Fibonacci Series: ");
         for(i=1;i<=n;i++){
             
-                System.out.print(n1+", "+(int)Math.pow(2,i)+", ");
+                System.out.print(number1+", "+(int)Math.pow(2,i)+", ");
                 
-                n3=n1+n2;
-                n1=n2;
-                n2=n3;
+                number3=number1+number2;
+                number1=number2;
+                number2=number3;
                 
         }
     }
 }
-class Q4{
+class Question4{
     public static void main(String[] args){
         int num;
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the number to print Fibonacci and Even number: ");
         num=sc.nextInt();
         
-        seq obj= new seq();
-        obj.even(num);
+        JavaSequence obj= new JavaSequence();
+        obj.EVEN(num);
     }
 }
