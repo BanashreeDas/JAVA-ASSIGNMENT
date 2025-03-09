b1d99294c68e439e297a7cecc887d9f155ea2aac
BanashreeDas <banashreed2@gmail.com>
2025-03-09 13:35:41 +0530

    Update and rename Q7.java to Question7.java

--- Question7.java
+++ Question7.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.*;
-class Seq
+class JavaSequence
 {
-	void sequ(int num){
+	void SEQ(int num){
 		int i, a=1;
 		for(i=1;i<=num;i++){
 		a*=i;
@@ -12,14 +12,14 @@ class Seq
 	}
 }
 
-class Q7{
+class Question7{
 
 	public static void main(String a[]){
 		int n;
 		Scanner sc= new Scanner(System.in);
 		System.out.print("Enter the number you want to print the sequence:");
 		n=sc.nextInt();
-		Seq obj=new Seq();
-		obj.sequ(n);
+		JavaSequence obj=new JavaSequence();
+		obj.SEQ(n);
 	}
 }
